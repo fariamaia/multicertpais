a8391e8955a1c6e187d3c5cab10b930202c6d410
Ricardo <Ricardo@DESKTOP-IMAMG4K.lan>
2023-12-14 00:16:11 +0000

    testing trigger in CI CD

--- src/main/java/com/rm/pais/internal/DataRepo.java
+++ src/main/java/com/rm/pais/internal/DataRepo.java
@@ -55,7 +55,7 @@ public class DataRepo {
 		
 		
 		//Create Scheduler to invoke the data refresh periodically
-		executorService.scheduleAtFixedRate(this::refresh, 0, 20, TimeUnit.SECONDS);
+		executorService.scheduleAtFixedRate(this::refresh, 0, 22, TimeUnit.SECONDS);
 	}
 	
 	
